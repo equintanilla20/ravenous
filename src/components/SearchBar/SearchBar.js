376cef830e85ee153b1d37513e6bdfd70ccf22c6
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2020-10-10 20:20:18 -0500

    Implemented limited keyboard support

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,6 +15,7 @@ class SearchBar extends React.Component {
 		this.handleLocationChange = this.handleLocationChange.bind(this);
 		this.handleSearch = this.handleSearch.bind(this);
 		this.handleSortByChange = this.handleSortByChange.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
 
 		this.sortByOptions = {
 			'Best Match': 'best_match',
@@ -48,6 +49,14 @@ class SearchBar extends React.Component {
 		event.preventDefault();
 	} // End handlerSearch
 
+	handleKeyPress(event) {
+		if(event.key === 'Enter') {
+			this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+
+		event.preventDefault();
+		};
+	}
+
 	renderSortByOptions() {
 		return Object.keys(this.sortByOptions).map(sortByOption => {
 			let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -69,8 +78,14 @@ class SearchBar extends React.Component {
 					</ul>
 				</div>
 				<div className="SearchBar-fields">
-					<input placeholder="Search Businesses" onChange={this.handleTermChange} />
-					<input placeholder="Where?" onChange={this.handleLocationChange}/>
+					<input 
+						placeholder="What are you in the mood for?" 
+						onChange={this.handleTermChange} 
+						onKeyUp={this.handleKeyPress} />
+					<input 
+						placeholder="Where? (City or ZIP)" 
+						onChange={this.handleLocationChange}
+						onKeyUp={this.handleKeyPress}/>
 				</div>
 				<div className="SearchBar-submit">
 					<a onClick={this.handleSearch}>Let's Go</a>
